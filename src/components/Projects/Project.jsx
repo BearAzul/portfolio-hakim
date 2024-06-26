5e414cf673a87c2602090d91833ce89b8a241082
BearAzul <adyaabduazizulhakim@gmail.com>
2024-06-26 17:26:20 +0700

    perbaikan deskripsi lengkap project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -18,7 +18,7 @@ const Project = () => {
                 Beberapa proyek terakhir
               </p>
             </div>
-            <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 md:gap-6">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap-6">
               {listProjects.map((project) => (
                 <div key={project.id}>
                   <div
@@ -28,27 +28,27 @@ const Project = () => {
                     data-aos="zoom-out-down"
                   >
                     <img src={project.image} alt={project.name} className="block object-contain w-full h-full" />
-                    <div className="overlay !w-full !h-full p-3 flex flex-col justify-end md:gap-5 gap-2 z-0">
-                      <h2 className="text-xs font-semibold text-gray-100 md:text-lg">
+                    <div className="overlay !w-full !h-full p-3 flex flex-col justify-between md:gap-5 gap-2 z-0">
+                      <h2 className="text-lg font-semibold text-gray-100">
                         {project.title}
                       </h2>
-                      <p className="text-[7px] text-justify md:text-sm text-white">
+                      <p className="text-sm text-justify text-white">
                         {project.description}
                       </p>
 
-                      <div className="flex flex-col justify-start gap-2 flex-center md:flex-row">
+                      <div className="flex justify-start gap-2 flex-center">
                         <a
                           href={project.demo}
                           className="btnDemo"
                           target="_blank"
                         >
                           <span>Demo</span>
-                          <i className="ri-external-link-line text-[10px] md:text-sm lg:text-base"></i>
+                          <i className="text-sm ri-external-link-line lg:text-base"></i>
                         </a>
 
                         <a href={project.sourceCode} className="btnDemo">
                           <span>Source Code</span>
-                          <i className="ri-github-fill text-[10px] md:text-sm lg:text-base"></i>
+                          <i className="text-sm ri-github-fill lg:text-base"></i>
                         </a>
                       </div>
                     </div>
