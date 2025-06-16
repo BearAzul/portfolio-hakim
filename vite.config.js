7e52a4c6242ffc61ca5f2abe5618100083cf245e
BearAzul <adyaabduazizulhakim@gmail.com>
2025-06-16 16:15:11 +0700

    fix error partytown

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { partytownVite } from "@qwik.dev/partytown/utils";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     partytownVite({
-      dest: "public/~partytown",
+      dest: path.join(__dirname, "dist", "~partytown"),
     }),
   ],
   assetsInclude: ["**/*.zip", "**/*.pdf"],
