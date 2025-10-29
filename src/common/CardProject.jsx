b56bc5c2782bdc39e1040e141ce25c5e2467b426
BearAzul <adyaabduazizulhakim@gmail.com>
2025-10-29 08:13:32 +0700

    mabok

--- src/common/CardProject.jsx
+++ src/common/CardProject.jsx
@@ -1,30 +1,28 @@
-import { useState } from "react";
+import { useId } from "react";
 
 const CardProject = ({ project }) => {
-  const [isImageVisible, setIsImageVisible] = useState(true);
-
-  const toggleImageVisibility = () => {
-    setIsImageVisible(!isImageVisible);
-  };
+  const zoomId = useId();
+  
   return (
     <div className="mb-6 border rounded-lg shadow-md border-slate-800 dark:border-accent break-inside-avoid">
+      <input type="checkbox" id={zoomId} className="hidden peer" />
       <div className="grid grid-cols-12">
-        <div className={`col-span-12 row-span-2  dark:border-accent border-slate-800
-                      transition-all duration-500 ease-in-out overflow-hidden
-                      ${isImageVisible ? "max-h-[500px] border-b" : "max-h-0"}`}>
-          <figure className="overflow-hidden rounded-t-lg aspect-3/2">
-            <img src={project.projectImage} alt={project.title} className="block object-cover w-full h-full transition-all hover:scale-125" />
-          </figure>
+        <div className="col-span-12 row-span-2 overflow-hidden transition-all duration-500 ease-in-out border-b dark:border-accent border-slate-800">
+          <label htmlFor={zoomId} className="cursor-pointer">
+            <figure className="overflow-hidden rounded-t-lg aspect-3/2">
+              <img src={project.projectImage} alt={project.title} className="block object-cover w-full h-full transition-all hover:scale-125" />
+            </figure>
+          </label>
         </div>
         <div className="relative col-span-12 p-3 space-y-2">
-          <button type="button" className="absolute -translate-x-1/2 border dark:border-accent border-primary hover:border-accent btn btn-sm btn-square -top-4 left-1/2 dark:btn-primary" onClick={toggleImageVisibility}>
-            <i
-              className={`text-lg ${isImageVisible
-                  ? "ri-arrow-drop-up-line"
-                  : "ri-arrow-drop-down-line"
-                }`}
-            ></i>
-          </button>
+          <div className="absolute flex items-center justify-center gap-2 -translate-x-1/2 -top-4 left-1/2">
+            <a href={project.projectUrl} target="_blank" className="px-2 py-1 text-sm transition-all bg-gray-100 border rounded-lg text-nowrap border-slate-800 hover:bg-slate-800 hover:text-gray-200 dark:bg-slate-800 dark:text-gray-200 dark:border-gray-200 dark:hover:text-slate-800 dark:hover:bg-gray-100">
+              <i className="text-sm ri-external-link-line lg:text-base"></i> Demo
+            </a>
+            <a href={project.repoUrl} target="_blank" className="px-2 py-1 text-sm transition-all bg-gray-100 border rounded-lg text-nowrap border-slate-800 hover:bg-slate-800 hover:text-gray-200 dark:bg-slate-800 dark:text-gray-200 dark:border-gray-200 dark:hover:text-slate-800 dark:hover:bg-gray-100">
+              <i className="text-sm ri-github-fill lg:text-base"></i> Source
+            </a>
+          </div>
           <h1 className="text-sm font-semibold dark:text-base-200">{project.title}</h1>
           <p className="text-xs text-justify dark:text-base-100">{project.description}</p>
         </div>
@@ -40,14 +38,21 @@ const CardProject = ({ project }) => {
             ))}
           </div>
         </div>
-        <div className="flex col-span-12 gap-2 p-3">
-          <a href={project.projectUrl} target="_blank" className="font-normal btn btn-primary btn-sm btn-outline dark:btn-accent" >
-            <i className="text-sm ri-external-link-line lg:text-base"></i> Demo
-          </a>
-          <a href={project.repoUrl} target="_blank" className="font-normal btn btn-sm btn-primary btn-outline dark:btn-accent">
-            <i className="text-sm ri-github-fill lg:text-base"></i> Source
-          </a>
-        </div>
+      </div>
+
+      <div
+        className="fixed inset-0 z-50 flex items-center justify-center invisible p-4 transition-all duration-300 opacity-0 bg-black/80 backdrop-blur-sm peer-checked:opacity-100 peer-checked:visible"
+      >
+        <img
+          src={project.projectImage}
+          alt={project.title}
+          className="block object-contain max-w-[90vw] max-h-[90vh] z-10"
+        />
+
+        <label
+          htmlFor={zoomId}
+          className="absolute inset-0 cursor-pointer"
+        ></label>
       </div>
     </div >
   );
