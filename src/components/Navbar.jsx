39ca8e8fd92ee39efcd0d405fa1c4c2e83c5d098
Adya Abdu Azizul Hakim <103102235+BearAzul@users.noreply.github.com>
2025-11-03 22:50:04 +0700

    Change Icons
    from remix to lucide: burger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import DarkMode from "../common/DarkMode.jsx"
-import { motion } from "framer-motion"
+import DarkMode from "../common/DarkMode.jsx";
+import { motion } from "framer-motion";
+import { Menu, X } from "lucide-react";
 
 const Header = () => {
   const [showNav, setVisible] = useState(false);
@@ -120,9 +121,7 @@ const Header = () => {
               onClick={toggleNav}
               aria-label="Toggle Menu"
             >
-              <i
-                className={`${showNav ? "ri-close-line" : "ri-menu-line"}`}
-              ></i>
+{showNav ? <Menu className="size-4" /> : <X className="size-4" />}
             </button>
           </div>
         </div>
