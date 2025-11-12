9742b6f3d0f5d0c697d35f6ee62e34d442bb623a
Adya Abdu Azizul Hakim <103102235+BearAzul@users.noreply.github.com>
2025-11-12 15:00:16 +0700

    fix
    years in experience

--- src/components/Qualifications.jsx
+++ src/components/Qualifications.jsx
@@ -79,6 +79,8 @@ const Qualifications = () => {
                   title={experience.company}
                   elemen={experience.position}
                   address={experience.location}
+
+years={experience.years}
                 />
               ) : (
                 <CardRight
