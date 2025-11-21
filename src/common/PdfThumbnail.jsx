e3b7b1cc60d13ea63386f436c0322388173d461d
BearAzul <adyaabduazizulhakim@gmail.com>
2025-11-21 19:46:13 +0700

    sheesh

--- src/common/PdfThumbnail.jsx
+++ src/common/PdfThumbnail.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
+import { LoaderCircle } from "lucide-react"
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -22,26 +23,25 @@ const PdfThumbnail = ({ fileUrl }) => {
   return (
     <div className="relative flex items-center justify-center object-cover w-full h-full bg-gray-50 dark:bg-gray-700">
 
-      {isLoading && <span className="loading loading-spinner text-secondary"></span>}
+      {isLoading && <LoaderCircle className='animate-spin size-8 text-secondary' />}
 
 
       {error && !isLoading && (
         <span className="p-2 text-xs text-center text-error">{error}</span>
       )}
 
-      <div className={isLoading || error ? 'hidden' : 'w-full h-full'}>
+      <div className={isLoading || error ? 'hidden' : 'w-full h-full flex items-center justify-center'}>
         <Document
           file={fileUrl}
           onLoadSuccess={onDocumentLoadSuccess}
           onLoadError={onDocumentLoadError}
-          loading=""
+          loading={null}
         >
           <Page
             pageNumber={1}
             renderTextLayer={false}
             renderAnnotationLayer={false}
-            className="flex items-center justify-center"
-            width={335}
+            width={330}
           />
         </Document>
       </div>
