d94956de268a3886a090c0ad4d32351f0cae7571
BearAzul <adyaabduazizulhakim@gmail.com>
2025-12-17 12:10:15 +0700

    save

--- src/common/MySkills/MySkills.jsx
+++ src/common/MySkills/MySkills.jsx
@@ -26,7 +26,7 @@ const HardSkills = ({ animate, skills }) => {
     >
       <div className="flex-none px-6">
         <h1 className="mb-6 font-bold md:-rotate-90 md:mb-0 text-slate-800 dark:text-gray-100">
-          <span className="mr-2 bg-teal-500 sm:hidden">&nbsp;</span>Teknologi
+          <span className="mr-2 bg-teal-500 sm:hidden">&nbsp;</span>Teknologi dan Alat
         </h1>
       </div>
 
