2203bd006db39b323f5c827eaa027db1ece71c1f
BearAzul <adyaabduazizulhakim@gmail.com>
2026-01-19 13:25:53 +0700

    Remove components useless

--- src/pages/CertificatePage.jsx
+++ src/pages/CertificatePage.jsx
@@ -1,10 +1,9 @@
 import { Link, useSearchParams } from "react-router"
 import { useDataStore } from "../store/useDataStore.js";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import DarkMode from "../common/DarkMode.jsx";
 import PdfThumbnail from "../common/PdfThumbnail.jsx";
 import { CircleArrowLeft, FileDown, StepForward, StepBack } from "lucide-react"
-import Loading from "../common/Loading.jsx";
 
 const CertificatePage = () => {
   const { certificates, totalPages, totalData, fetchCertificates} = useDataStore();
