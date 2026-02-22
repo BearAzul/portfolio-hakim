a6054adeaa498e3a362e77aa4fa071c0379d310b
BearAzul <adyaabduazizulhakim@gmail.com>
2026-02-22 16:03:47 +0700

    More z index

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,7 +5,7 @@ const Support = () => {
     window.qr_modal.showModal();
   }
   return (
-    <button type="button" aria-label="qr_btn" className="fixed bottom-80 lg:bottom-70 -right-20 -rotate-90 btn btn-neutral h-[60px] flex items-start cursor-pointer z-20 shadow-lg" onClick={openModal}>
+    <button type="button" aria-label="qr_btn" className="fixed bottom-80 lg:bottom-70 -right-20 -rotate-90 btn btn-neutral h-[60px] flex items-start cursor-pointer z-40 shadow-lg" onClick={openModal}>
       <span>Buy Me a Coffee</span>
     </button>
   )
