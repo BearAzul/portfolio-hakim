3cd237834dc1a993cac93f5cf1cc043783d1562c
BearAzul <adyaabduazizulhakim@gmail.com>
2026-03-22 18:56:47 +0700

    animate popup support

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,13 +1,26 @@
 import { Coffee } from "lucide-react"
+import { motion } from "framer-motion"
 
 const Support = () => {
   const openModal = () => {
     window.qr_modal.showModal();
   }
+
   return (
-    <button type="button" aria-label="qr_btn" className="fixed bottom-80 lg:bottom-70 -left-20 rotate-90 btn btn-sm btn-secondary h-[60px] flex items-start cursor-pointer z-40 shadow-lg font-medium" onClick={openModal}>
+    <motion.button type="button" aria-label="qr_btn" className="fixed bottom-80 lg:bottom-70 -left-20 rotate-90 btn btn-sm btn-secondary h-[60px] flex items-start cursor-pointer z-40 shadow-lg font-medium" onClick={openModal}
+      initial={{
+        opacity: 0,
+        y: 20
+      }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        type: "spring", stiffness: 300,
+        damping: 10,
+        delay: 1.5
+      }}
+    >
       <span className="flex items-center py-1">Buy Me a Coffee <Coffee className="size-5 ml-2" /></span>
-    </button>
+    </motion.button>
   )
 }
 
