a206e5e55919479b104d0754e43e1781fbd435f1
BearAzul <adyaabduazizulhakim@gmail.com>
2026-03-22 18:32:57 +0700

    remove border

--- src/components/StairsEffect.jsx
+++ src/components/StairsEffect.jsx
@@ -28,7 +28,7 @@ const StairsEffect = () => {
               ease: "easeInOut",
               delay: reverseIndex(i) * 0.1
             }}
-            className="h-full w-full dark:bg-teal-600 bg-slate-800 relative border-t-4 dark:border-gray-100 border-teal-600"
+            className="h-full w-full dark:bg-teal-600 bg-slate-800 relative"
           />
         ))}
       </div>
