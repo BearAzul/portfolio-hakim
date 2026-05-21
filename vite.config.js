97684751e627b4fee07355ecc25abec0f9f49e74
bearazul <adyaabduazizulhakim@gmail.com>
2026-05-21 14:06:29 +0700

    fix: update index.html meta tags and add Google Tag Manager scripts; refine vite.config.js configuration

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { partytownVite } from "@qwik.dev/partytown/utils";
+import { partytownVite } from "@qwik.dev/partytown/utils";
 import tailwindcss from "@tailwindcss/vite";
-// import path from "path";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // partytownVite({
-    //   dest: path.join(__dirname, "dist", "~partytown"),
-    // }),
+    partytownVite({
+      dest: path.join(__dirname, "dist", "~partytown"),
+    }),
   ],
   assetsInclude: ["**/*.zip", "**/*.pdf", "**/*.lottie"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
