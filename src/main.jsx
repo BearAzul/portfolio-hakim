3105c5afe6ded519e83fb7caaa5d44fe4ad2626e
bearazul <adyaabduazizulhakim@gmail.com>
2026-05-21 14:17:06 +0700

    fix: update pdf.worker.min.mjs URL to use HTTPS for improved security

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { pdfjs } from "react-pdf";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
