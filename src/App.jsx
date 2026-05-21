0b19196a4ccd42c44a5639ff0b88932f1b5d6417
bearazul <adyaabduazizulhakim@gmail.com>
2026-05-21 11:57:13 +0700

    chore: update React and related dependencies to version 19.x and refactor App component for improved SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async"
 import CustomCursor from "./common/CustomCursor.jsx";
 import Footer from "./components/Footer.jsx";
 import Support from "./components/Support.jsx";
@@ -6,35 +5,61 @@ import QRCode from "./common/QRCode.jsx";
 import BtnTop from "./common/BtnTop.jsx";
 import AnimatedRoutes from "./Routes/AnimatedRoutes.jsx";
 
-
 const App = () => {
   return (
     <>
-      <Helmet>
-        <title>Adya Abdu Azizul Hakim — Frontend Developer</title>
-        <meta name="description" content="Portofolio resmi Adya Abdu Azizul Hakim, seorang Frontend Web Developer yang berfokus pada pengembangan aplikasi modern dengan teknologi React.js, Node.js, dan Next.js" />
-        <meta name="keywords" content="Adya Abdu Azizul Hakim, Web Developer, Frontend Developer, React, Node.js, Next.js, Portofolio Frontend Developer Kampung" />
-        <meta name="author" content="Adya Abdu Azizul Hakim" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <link rel="canonical" href="https://akimdev.vercel.app" />
-        <meta name="robots" content="index, follow" />
+      <title>Adya Abdu Azizul Hakim — Frontend Developer</title>
+      <meta
+        name="description"
+        content="Portofolio resmi Adya Abdu Azizul Hakim, seorang Frontend Web Developer yang berfokus pada pengembangan aplikasi modern dengan teknologi React.js, Node.js, dan Next.js"
+      />
+      <meta
+        name="keywords"
+        content="Adya Abdu Azizul Hakim, Web Developer, Frontend Developer, React, Node.js, Next.js, Portofolio Frontend Developer Kampung"
+      />
+      <meta name="author" content="Adya Abdu Azizul Hakim" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="canonical" href="https://akimdev.vercel.app" />
+      <meta name="robots" content="index, follow" />
 
-        <meta property="og:title" content="Adya Abdu Azizul Hakim — Frontend Developer" />
-        <meta property="og:description" content="Kunjungi portofolio profesional saya yang menampilkan proyek, keahlian, dan pengalaman sebagai Frontend Developer." />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://akimdev.vercel.app" />
-        <meta property="og:image" content="https://avatars.githubusercontent.com/u/103102235?v=4" />
-        <meta property="og:site_name" content="AkimDev" />
+      <meta
+        property="og:title"
+        content="Adya Abdu Azizul Hakim — Frontend Developer"
+      />
+      <meta
+        property="og:description"
+        content="Kunjungi portofolio profesional saya yang menampilkan proyek, keahlian, dan pengalaman sebagai Frontend Developer."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://akimdev.vercel.app" />
+      <meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/103102235?v=4"
+      />
+      <meta property="og:site_name" content="AkimDev" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Adya Abdu Azizul Hakim — Frontend Developer" />
-        <meta name="twitter:description" content="Portofolio resmi Adya Abdu Azizul Hakim, seorang pengembang web profesional." />
-        <meta name="twitter:image" content="https://avatars.githubusercontent.com/u/103102235?v=4" />
-        <meta name="twitter:creator" content="@adya_abdu77" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:title"
+        content="Adya Abdu Azizul Hakim — Frontend Developer"
+      />
+      <meta
+        name="twitter:description"
+        content="Portofolio resmi Adya Abdu Azizul Hakim, seorang pengembang web profesional."
+      />
+      <meta
+        name="twitter:image"
+        content="https://avatars.githubusercontent.com/u/103102235?v=4"
+      />
+      <meta name="twitter:creator" content="@adya_abdu77" />
 
-        <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/103102235?v=4" type="image/x-icon" />
-      </Helmet>
+      <link
+        rel="shortcut icon"
+        href="https://avatars.githubusercontent.com/u/103102235?v=4"
+        type="image/x-icon"
+      />
+
       <CustomCursor />
 
       <AnimatedRoutes />
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "animate.css"
-import './index.css'
-import { HelmetProvider } from "react-helmet-async"
-import { pdfjs } from 'react-pdf';
-import { BrowserRouter } from "react-router"
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "animate.css";
+import "./index.css";
+import { pdfjs } from "react-pdf";
+import { BrowserRouter } from "react-router";
+import App from "./App.jsx";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
