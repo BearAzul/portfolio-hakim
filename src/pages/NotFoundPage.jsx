c119007039e548e534e1da250774c0065b529e44
bearazul <adyaabduazizulhakim@gmail.com>
2026-05-21 13:28:53 +0700

    fix: add relative positioning to container in NotFoundPage for layout consistency

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -12,7 +12,7 @@ const NotFoundPage = () => {
       className="flex items-start justify-center w-full h-dvh py-10 bg-gray-200 dark:bg-slate-800"
     >
       <DotPattern />
-      <div className="container px-6 mx-auto md:max-w-2xl lg:max-w-5xl md:px-4">
+      <div className="container px-6 mx-auto md:max-w-2xl lg:max-w-5xl md:px-4 relative">
         <div className="flex items-center justify-between">
           <Link
             to="/"
