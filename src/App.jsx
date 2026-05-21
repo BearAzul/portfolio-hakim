771a918457cddb4719a6c888277c15a090214344
bearazul <adyaabduazizulhakim@gmail.com>
2026-05-21 14:57:56 +0700

    fix: add canonical link and Open Graph URL to improve SEO

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import Support from "./components/Support.jsx";
 import QRCode from "./common/QRCode.jsx";
 import BtnTop from "./common/BtnTop.jsx";
 import AnimatedRoutes from "./Routes/AnimatedRoutes.jsx";
+import { useLocation } from "react-router";
 
 const App = () => {
+  const location = useLocation();
+
+  const currentUrl = `https://akimdev.vercel.app${location.pathname}`;
+
   return (
     <>
+      <link rel="canonical" href={currentUrl} />
+      <met property="og:url" content={currentUrl} />
+
       <CustomCursor />
 
       <AnimatedRoutes />
